41e03a812434888d2fd61d050ee8e6591c657d1e
yanghui5889 <yanghui5889@163.com>
2020-10-28 22:08:59 +0800

    commit 4

--- re_ip.py
+++ re_ip.py
@@ -1,6 +1,13 @@
 import re
 str1 = 'Port-channel1.189    192.168.189.254  Yes     CONFIG   up'
-result = re.match('(\w+.\w+.\d+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+Yes\s+CONFIG\s+(\w+)',str1).groups()
-print('接口    :'+result[0])
-print('IP地址  :'+result[1])
-print('状态    :'+result[2])
+result = re.match(r'(\w+.\w+.\d+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+Yes\s+CONFIG\s+(\w+)',
+                  str1.strip()).groups()
+
+format_str = '{0:<10}:{1:<30}'
+print(format_str.format('接口',result[0]))
+print(format_str.format('IP地址',result[1]))
+print(format_str.format('状态',result[2]))
+
+# print('接口    :'+result[0])
+# print('IP地址  :'+result[1])
+# print('状态    :'+result[2])
